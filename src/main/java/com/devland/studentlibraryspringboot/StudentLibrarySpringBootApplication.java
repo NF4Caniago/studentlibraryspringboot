ea03549bb760807c60eb6e3e96145d6d7ae9f3ee
Afif <afifilhamcaniago@gmail.com>
2021-10-22 19:48:54 +0700

    first-commit

---
+++ src/main/java/com/devland/studentlibraryspringboot/StudentLibrarySpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.devland.studentlibraryspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentLibrarySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentLibrarySpringBootApplication.class, args);
+	}
+
+}
